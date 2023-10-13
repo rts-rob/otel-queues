6245e82e01970702a2fdcb3034756c321637dbad
Rob Sutter <rsutter@cloudflare.com>
2023-10-13 13:03:29 +0200

    Initial commit.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,12 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Queue consumer: a Worker that can consume from a
- * Queue: https://developers.cloudflare.com/queues/get-started/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+import { instrument, ResolveConfigFn } from '@microlabs/otel-cf-workers';
 export interface Env {
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
 	MY_QUEUE: Queue;
+	BASELIME_API_KEY: string;
 }
 
-export default {
-	// Our fetch handler is invoked on a HTTP request: we can send a message to a queue
-	// during (or after) a request.
-	// https://developers.cloudflare.com/queues/platform/javascript-apis/#producer
+const handler = {
 	async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// To send a message on a queue, we need to create the queue first
-		// https://developers.cloudflare.com/queues/get-started/#3-create-a-queue
+		// Producer Worker - places messages onto the queue
 		await env.MY_QUEUE.send({
 			url: req.url,
 			method: req.method,
@@ -30,14 +14,22 @@ export default {
 		});
 		return new Response('Sent message to the queue');
 	},
-	// The queue handler is invoked when a batch of messages is ready to be delivered
-	// https://developers.cloudflare.com/queues/platform/javascript-apis/#messagebatch
+	// Consumer Worker - processes batches of messages from the queue
 	async queue(batch: MessageBatch<Error>, env: Env): Promise<void> {
-		// A queue consumer can make requests to other endpoints on the Internet,
-		// write to R2 object storage, query a D1 Database, and much more.
 		for (let message of batch.messages) {
-			// Process each message (we'll just log these)
 			console.log(`message ${message.id} processed: ${JSON.stringify(message.body)}`);
 		}
 	},
 };
+
+const config: ResolveConfigFn = (env: Env, _trigger) => {
+	return {
+		exporter: {
+			url: 'https://otel.baselime.io/v1',
+			headers: { 'x-api-key': env.BASELIME_API_KEY },
+		},
+		service: { name: 'otel-queues' },
+	}
+};
+
+export default instrument(handler, config);
